bad76878560e6b35eaf52d53182107eded7d7eb9
Hanzhi11 <hanzhizhang11@gmail.com>
2022-09-19 23:28:22 +1000

    completed new list creation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class ItemContent:
 
     def __repr__(self):
         return f'Item name: {self.name}, Priority: {self.priority}, Due on: {self.due_date}'
+
 class PriorityError(Exception):
     def __init__(self):
         super().__init__('Wrong priority level!')
@@ -44,19 +45,26 @@ def add_item(list_name, item):
     item_name = item_content[0].lower()
     item_priority_level = item_content[1].lower()
     item_due_date = item_content[2]
-    convert_due_date = datetime.strptime(item_content[2], '%d/%m/%y').date()
-    default_due_date= date.today() + timedelta(days = 1)
+    default_due_date= (date.today() + timedelta(days = 1)).strftime('%d/%m/%y')
 
     if len(item_priority_level) == 0 and len(item_content[2]) != 0:
-        item_content_details = ItemContent(item_name, 'medium', convert_due_date)
+        convert_due_date = datetime.strptime(item_content[2], '%d/%m/%y').date()
+        item_content_details = ItemContent(item_name, '2', convert_due_date)
     elif len(item_priority_level) == 0 and len(item_content[2]) == 0:
-        item_content_details = ItemContent(item_name, 'medium', default_due_date)
+        item_content_details = ItemContent(item_name, '2', default_due_date)
+    elif len(item_priority_level) != 0 and len(item_content[2]) == 0:
+        item_content_details = ItemContent(item_name, item_priority_level, default_due_date)
     else:
         item_content_details = ItemContent(item_name, item_priority_level, item_due_date)
 
-    items.append(item_content_details)
-    items_list = dict(enumerate(items))
-    new_list = {list_name: items_list}
+    item_details = {'Name': item_content_details.name, 'Priority': item_content_details.priority, 'Due date':item_content_details.due_date}
+    print(item_details)
+
+    items.append(item_details)
+    # items_list = dict(enumerate(items))
+    sorted_item_list = sorted(items, key = lambda item: (item['Priority'], item['Due date']), reverse = True )
+    print(sorted_item_list)
+    new_list = {list_name: sorted_item_list}
     list_collection.update(new_list)
     print(list_collection)
 
@@ -64,31 +72,29 @@ def add_item(list_name, item):
 def name_and_priority_check(item):
     item_content = item.split(',')
     if item.count(',') == 2 and len(item_content[0]) != 0:
-        if len(item_content[1]) != 0 and item_content[1] not in ['high', 'medium', 'low']:
+        if len(item_content[1]) != 0 and item_content[1] not in ['1', '2', '3']:
             raise PriorityError
         else:
             return item
     else:
         raise InvalidInputError
 
+
 def validate_and_add(list_name):
     input_is_valid = False
     while not input_is_valid:
         item = input('Enter the item\'s name, priority and due date DD/MM/YY (X to exit the app or Q to back): ')
-        check_result = quit_check_main(item)
-        if check_result == 'back to main':
-            raise BackToMain
-        else:
-            try:
-                name_and_priority_check(item)
-                add_item(list_name, item)
-                input_is_valid = True
-            except PriorityError as err:
-                print(err)
-            except InvalidInputError as err:
-                print(err)
-            except ValueError:
-                print('Invalid date!')
+        quit_check_main(item)
+        try:
+            name_and_priority_check(item)
+            add_item(list_name, item)
+            input_is_valid = True
+        except PriorityError as err:
+            print(err)
+        except InvalidInputError as err:
+            print(err)
+        except ValueError:
+            print('Invalid date!')
 
 
 try:
